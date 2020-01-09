668e6e3272c88cfd7cfc7997fa3576fcbf3960cc
malgorzatagwinner <malgorzata.gwinner@gmail.com>
2020-01-09 23:11:48 +0100

    Poprawiona wersja

--- ZADANIA_11/quicksort_iter.py
+++ ZADANIA_11/quicksort_iter.py
@@ -11,20 +11,16 @@ def partition(lista, begin, r):
     lista[a+1], lista[r] = lista[r], lista[a+1]
     return a+1
 
-def sorted(lista, begin, r):
-    for i in range(begin, r):
-        if(lista[i] > lista[i+1]):
-            return False
-    return True
-
 def quicksort_iter(lista, begin, r):
     stos = [(begin, r)]
     while len(stos)!=0:
         a, b = stos[-1]
         stos.pop(-1)
-        if( not sorted(lista, a, b)):
-            q = partition(lista, a, b)
+        q = partition(lista, a, b)
+            
+        if q-1 > a:
             stos.append((a, q-1))
+        if q+1 < b:
             stos.append((q+1, b))
 
 if __name__ == '__main__':
